f878cb41f2f873e8d42bf6d7d4b467e7e710d693
blibotics <blibotics@gmail.com>
2013-06-16 14:57:13 -0400

    Starting Homework Assignment, Completed squareNumber Function

---
+++ 01-lesson_hwk-calculator.js
@@ -0,0 +1,17 @@
+// Lesson: 1
+// Homework: The Calculator
+// URL: http://www.teaching-materials.org/javascript/exercises/calculator.html
+
+// The Calculator
+
+// #1 squareNumber function
+function squareNumber(num){
+    var squared = num*num;
+    console.log("The result of squaring the number " + num + " is " + squared + ".");
+    
+}
+
+squareNumber(3);
+
+//--------------------------------------------------------------------------------------//
+//--------------------------------------------------------------------------------------//
