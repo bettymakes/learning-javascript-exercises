bab32c62cab17f11796cd6be38447512230eeef5
blibotics <blibotics@gmail.com>
2013-06-16 19:46:46 -0400

    Started and Completed Arrays Exercise

---
+++ 02-lesson_exercise-arrays.js
@@ -0,0 +1,15 @@
+// Lesson: 2
+// Exercises: Arrays
+// URL: http://www.teaching-materials.org/javascript/exercises/arrays.html
+
+//Your Top Choices
+var topChoices = ["Power Rangers", "Digimon", "Teletubbies", "SpongeBob", "Arthur"];
+
+for (var choice = 0; choice <= topChoices.length; choice ++) {
+	console.log("My #1 choice is " + topChoices[choice] + ".");
+}
+
+
+
+//--------------------------------------------------------------------------------------//
+//--------------------------------------------------------------------------------------//
