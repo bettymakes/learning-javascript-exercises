0500d55177d61093605729b24969683dfac45058
blibotics <blibotics@gmail.com>
2013-06-16 14:44:10 -0400

    Completed Age Calculator Exercise

--- 01-lesson_exercise-functions.js
+++ 01-lesson_exercise-functions.js
@@ -4,7 +4,7 @@
 
 // The Fortune Teller
 function tellFortune(kids, name, place, job) {
-      console.log("You will be a " + job + " in " + place + ", and married to " + name + " with " + kids + " kids.");
+	console.log("You will be a " + job + " in " + place + ", and married to " + name + " with " + kids + " kids.");
 }
 
  tellFortune(10, "Polkaroo", "Polka Land", "Polkadot-Shorts-Wearer");
@@ -16,3 +16,36 @@ function tellFortune(kids, name, place, job) {
 
 //--------------------------------------------------------------------------------------//
 //--------------------------------------------------------------------------------------//
+
+//The Age Calculator
+function calculateAge(birthYear, currentYear){
+	var age = currentYear - birthYear;
+	console.log("You are either " + (age-1) + " or " + age + ".");
+}
+
+calculateAge(2003, 2013);
+calculateAge(1946, 2050);
+calculateAge(1960, 1980);
+
+//BONUS: Figure out how to get current year in Javascript instead of passing it in
+function calculateAge(birthYear){
+//Created a Date object, called the function getFullYear on it.
+//Assigned it to the variable current Year.
+	var currentYear = new Date().getFullYear();
+	var age = currentYear - birthYear;
+	var message = ("You are either " + (age-1) + " or " + age + ".");
+	console.log(message);
+}
+
+calculateAge(2003);
+
+// Website's solutions and mine are basically the same.
+// WOOP WOOP! :)
+
+//--------------------------------------------------------------------------------------//
+//--------------------------------------------------------------------------------------//
+
+
+
+
+
