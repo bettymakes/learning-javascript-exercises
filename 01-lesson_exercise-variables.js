ce83f6c8e7d291ee983e59152ae7213ed8837c22
blibotics <blibotics@gmail.com>
2013-06-16 13:55:20 -0400

    First Commit

---
+++ 01-lesson_exercise-variables.js
@@ -0,0 +1,14 @@
+// Lesson: 1
+// Exercises: Variables
+// URL: http://www.teaching-materials.org/javascript/exercises/variables.html
+
+
+//The Fortune Teller
+function fortune(kids, name, place, job) {
+      console.log("You will be a " + job + " in " + place + ", and married to " + name + " with " + kids + " kids.");
+}
+
+ fortune(10, "Polkaroo", "Toronto", "developper");
+
+
+//The Age Calculator
---
+++ README.md
@@ -0,0 +1,12 @@
+Learning Javascript!
+
+Teaching Materials: Intro to Javascript
+http://www.teaching-materials.org/javascript/
+
+
+Lesson 1: Variables & Functions
+Lesson 2: Control Flow & Arrays
+Lesson 3: Objects & DOM
+Lesson 4: Animation & Events
+
+My answers will be made available as I go through the exercises.
