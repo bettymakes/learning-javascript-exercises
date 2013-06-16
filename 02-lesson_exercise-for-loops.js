7ffcc229bc0022c386c8bcce1f13acc6017e4c92
blibotics <blibotics@gmail.com>
2013-06-16 19:18:44 -0400

    Completed Multiplication Tables Exercise

--- 02-lesson_exercise-for-loops.js
+++ 02-lesson_exercise-for-loops.js
@@ -4,6 +4,33 @@
 
 
 // The Even/Odd Reporter
+for (i=0; i<=20; i++) {
+	if (i == 0) {
+		console.log(i + " is even, but some argue it's not?");
+	}
+	else if (i % 2 == 0) {
+		console.log(i + " is even");   
+	}
+	else {
+		console.log(i + " is odd");
+	}
+}
+
+
+//--------------------------------------------------------------------------------------//
+//--------------------------------------------------------------------------------------//
+
+//Multiplication Tables
+for (i=0; i<=10; i++) {
+	console.log(i + " * 9 = " + i*9);
+}
+
+//BONUS: Use a nested for loop to show the tables for every multiplier from 1 to 10 (100 results total).
+for (i=1; i<=10; i++) {
+	for (x=1; x<=10; x++) {
+		console.log(i + " times " + x + " is " + (i*x) + ".");
+    }
+}
 
 //--------------------------------------------------------------------------------------//
 //--------------------------------------------------------------------------------------//
