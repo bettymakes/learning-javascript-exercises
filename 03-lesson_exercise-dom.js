7136b484b2c1a4829cc92db179fa86a61791ff75
blibotics <blibotics@gmail.com>
2013-06-17 14:34:26 -0400

    Started DOM exercises, Completed Logo Hijack

---
+++ 03-lesson_exercise-dom.js
@@ -0,0 +1,16 @@
+// Lesson: 3
+// Exercises: The DOM
+// URL: http://www.teaching-materials.org/javascript/exercises/dom.html
+
+
+//Logo Hijack
+
+//'get'ting the ElementById
+googleLogo = document.getElementById("hplogo");
+
+//'set'ting the Attribute for googleLogo (we got from line 9)
+//'set'ting new Attributes
+googleLogo.setAttribute("style", "background: url(http://images.wikia.com/powerrangers/images/2/2f/Logo_Power_Rangers_1_MMPR_Era.png) no-repeat; background-size:250px 90px; height:90px; width:250px");
+
+//--------------------------------------------------------------------------------------//
+//--------------------------------------------------------------------------------------//
