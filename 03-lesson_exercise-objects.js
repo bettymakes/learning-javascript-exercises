9f8a23e8a160829e8043fe437e497097391365da
blibotics <blibotics@gmail.com>
2013-06-17 02:25:53 -0400

    Completed Reading List Exercise

--- 03-lesson_exercise-objects.js
+++ 03-lesson_exercise-objects.js
@@ -21,3 +21,52 @@ console.log(favRecipe.ingredients[2]);
 
 //--------------------------------------------------------------------------------------//
 //--------------------------------------------------------------------------------------//
+
+//Recipe Card
+var booksArray = [
+	{ 
+		bookTitle: "G Eggs Ham",
+		author: "Seussy",
+		alreadyRead: true 
+	}, 
+	{
+		bookTitle: "Love Forever",
+		author: "R. Munsch-kin",
+		alreadyRead: true
+	},
+	{
+		bookTitle: "Chicken Bumps",
+		author: "RL Steinburg",
+		alreadyRead: false
+	}
+]
+
+for (var index = 0; index < booksArray.length; index ++) {
+	var indexBookTitle = booksArray[index].bookTitle
+	var indexAuthor = booksArray[index].author
+
+	console.log(indexBookTitle + " by " + indexAuthor);
+	if (booksArray[index].alreadyRead) {
+		console.log("You already read " + indexBookTitle + " by " + indexAuthor + ".");
+	}
+	else {
+		console.log("You still need to read " + indexBookTitle + " by " + indexAuthor + ".");
+	}
+}
+
+//--------------------------------------------------------------------------------------//
+//--------------------------------------------------------------------------------------//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
