6313aba22a72857c0fcadf12fbf99d23aedfbecb
blibotics <blibotics@gmail.com>
2013-06-17 02:10:50 -0400

    Started Lesson 3, Objects - Recipe Card Exercise Completed

---
+++ 03-lesson_exercise-objects.js
@@ -0,0 +1,23 @@
+// Lesson: 3
+// Exercises: Objects
+// URL: http://www.teaching-materials.org/javascript/exercises/objects.html
+
+
+//Recipe Card
+
+var favRecipe = {
+	title: "Green Eggs and Ham",
+	servings: 2,
+	ingredients: ["2x Green Eggs", "4x Ham", "TLC"]
+}
+
+console.log(favRecipe.title);
+console.log("Serves: " + favRecipe.servings);
+console.log("Ingredients:");
+console.log(favRecipe.ingredients[0]);
+console.log(favRecipe.ingredients[1]);
+console.log(favRecipe.ingredients[2]);
+
+
+//--------------------------------------------------------------------------------------//
+//--------------------------------------------------------------------------------------//
