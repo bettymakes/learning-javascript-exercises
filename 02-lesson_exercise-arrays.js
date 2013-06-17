21e7647a8b763e21119e114214ef02d0f2661716
blibotics <blibotics@gmail.com>
2013-06-16 20:02:14 -0400

    Completed Bonus Exercise in Arrays

--- 02-lesson_exercise-arrays.js
+++ 02-lesson_exercise-arrays.js
@@ -5,8 +5,32 @@
 //Your Top Choices
 var topChoices = ["Power Rangers", "Digimon", "Teletubbies", "SpongeBob", "Arthur"];
 
-for (var choice = 0; choice <= topChoices.length; choice ++) {
-	console.log("My #1 choice is " + topChoices[choice] + ".");
+for (var choice = 0; choice < topChoices.length; choice ++) {
+	var choicePosition = choice+1;
+	console.log("My #" + choicePosition + " choice is " + topChoices[choice] + ".");
+}
+
+// Bonus: Change it to log "My 1st choice, "My 2nd choice", "My 3rd choice", 
+//        picking the right suffix for the number based on what it is.
+var topChoices = ["Power Rangers", "Digimon", "Teletubbies", "SpongeBob", "Arthur", "SpiderMan"];
+
+for (var choice = 0; choice < topChoices.length; choice ++) {
+	var choicePosition = choice+1;
+	var endWord;
+	if (choicePosition == 1) {
+		endWord = "st";
+	}
+	else if (choicePosition == 2) {
+		endWord = "nd";
+	}
+	else if (choicePosition == 3) {
+		endWord = "rd";
+	}
+	else {
+		endWord = "th";
+	}
+
+	console.log("My " + choicePosition + endWord + " choice is " + topChoices[choice] + ".");
 }
 
 
