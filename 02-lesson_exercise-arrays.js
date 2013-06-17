e28f0c5673670d1faa47148b2c29f20b4ce776a1
blibotics <blibotics@gmail.com>
2013-06-16 20:06:20 -0400

    Took up the answers, compared to Solution Sheet on Website, comments included

--- 02-lesson_exercise-arrays.js
+++ 02-lesson_exercise-arrays.js
@@ -10,6 +10,13 @@ for (var choice = 0; choice < topChoices.length; choice ++) {
 	console.log("My #" + choicePosition + " choice is " + topChoices[choice] + ".");
 }
 
+//Basically did the same thing as the Solution Sheet! Woop!
+//Only difference was that I had assigned the choice position to a variable
+//Also, discovered an issue earlier while working on Bonus question ...
+//Revised the condition of the for loop
+
+
+
 // Bonus: Change it to log "My 1st choice, "My 2nd choice", "My 3rd choice", 
 //        picking the right suffix for the number based on what it is.
 var topChoices = ["Power Rangers", "Digimon", "Teletubbies", "SpongeBob", "Arthur", "SpiderMan"];
@@ -33,7 +40,8 @@ for (var choice = 0; choice < topChoices.length; choice ++) {
 	console.log("My " + choicePosition + endWord + " choice is " + topChoices[choice] + ".");
 }
 
-
+//Yay!!! Looks the same as the Solution Sheet. I'm Awesome!! 
+//Ok not really ... I'm just trying to boost my own confidence lol.
 
 //--------------------------------------------------------------------------------------//
 //--------------------------------------------------------------------------------------//
